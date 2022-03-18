c1113ccd04d8bfc03877a5fd70a86b8104c15fb4
Arseniy Graur <argraur@gmail.com>
2022-03-18 14:11:59 +0300

    aosp-builder: Add MIT License

--- src/main/kotlin/dev/argraur/aosp/builder/config/BuildConfig.kt
+++ src/main/kotlin/dev/argraur/aosp/builder/config/BuildConfig.kt
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Arseniy Graur <me@argraur.dev>
+ * SPDX-License-Identifier: MIT
+ */
+
 package dev.argraur.aosp.builder.config
 
 import dev.argraur.aosp.builder.utils.Logger
